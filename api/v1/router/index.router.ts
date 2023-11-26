ef23f44f4115d896ded43ecdc6104738f252a330
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-26 13:12:10 +0700

    Chuyển các router thành private

--- api/v1/router/index.router.ts
+++ api/v1/router/index.router.ts
@@ -1,10 +1,11 @@
 import taskRoutes from "./task.route";
 import userRoutes from "./user.router";
 import { Express } from "express";
+import * as authMiddleware from "../../v1/middleware/authen.middleware";
 
 const mainV1Router = (app: Express): void=> {
     const version = "/api/v1";
-    app.use(version + "/tasks", taskRoutes);
+    app.use(version + "/tasks",authMiddleware.requireAuth ,taskRoutes);
     app.use(version + "/users", userRoutes);
 }
 
