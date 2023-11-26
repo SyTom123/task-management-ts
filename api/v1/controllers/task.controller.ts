3a57d12f10f8713b73632ff54562e00245c74dfb
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-26 07:48:31 +0700

    Bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,17 @@ import Task from "../model/task.model"
 import {Request, Response} from 'express';
 
 export const index = async (req: Request, res: Response) => {
-    const tasks = await Task.find({
+    interface Find {
+        deleted: boolean,
+        status?: string,
+    }
+    const find: Find = {
         deleted: false
-    })
+    };
+    if(req.query.status) {
+        find.status= req.query.status.toString();
+    }
+    const tasks = await Task.find(find);
 
     res.json(tasks)
 }
